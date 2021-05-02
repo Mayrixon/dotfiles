d62a062e80ac23ed2fc873838425ba90164c8c78
Mayrixon <bygsxizhen@gmail.com>
2021-05-02 21:35:41 +0100

    Update setting up configs for nvim.

--- neovim-0.5/.config/nvim/lua/settings.lua
+++ neovim-0.5/.config/nvim/lua/settings.lua
@@ -3,33 +3,58 @@ local utils = require('utils')
 local cmd = vim.cmd
 local indent = 4
 
--- Map leader and local leader keys
-vim.g.mapleader = ' '
-vim.g.maplocalleader = '\\'
-
 -- Remote providers
 vim.g.loaded_python_provider = 0
 vim.g.python3_host_prog = '/usr/bin/python3'
 
 cmd 'syntax enable'
 cmd 'filetype plugin indent on'
+
+-- Global settings
+utils.opt('o', 'ttimeoutlen', 100)
+
+utils.opt('o', 'mouse', 'a')
+
+utils.opt('o', 'hidden', true)
+
+utils.opt('o', 'display', 'msgsep')
+
+utils.opt('o', 'ignorecase', true)
+utils.opt('o', 'smartcase', true)
+utils.opt('o', 'inccommand', 'nosplit')
+
+utils.opt('o', 'lazyredraw', true)
+-- utils.opt('o', 'synmaxcol', 500)
+
+utils.opt('o', 'wildmode', 'longest,full')
+utils.opt('o', 'shortmess', vim.o.shortmess .. 'c')
+
+utils.opt('o', 'showmatch', true)
+
+
+-- Window-local settings
+utils.opt('w', 'scrolloff', 4 )
+utils.opt('w', 'sidescrolloff', 4 )
+
+utils.opt('w', 'listchars', 'tab:> ,trail:-,extends:>,precedes:<,nbsp:+')
+utils.opt('w', 'list', true)
+
+utils.opt('w', 'number', true)
+utils.opt('w', 'relativenumber', true)
+utils.opt('w', 'signcolumn', 'yes')
+
+utils.opt('w', 'foldmethod', 'syntax')
+
+utils.opt('w', 'conceallevel', 1)
+utils.opt('w', 'concealcursor', 'nc')
+
+
+-- Buffer-local settings
 utils.opt('b', 'expandtab', true)
 utils.opt('b', 'shiftwidth', indent)
 utils.opt('b', 'smartindent', true)
 utils.opt('b', 'tabstop', indent)
-utils.opt('o', 'hidden', true)
-utils.opt('o', 'ignorecase', true)
-utils.opt('o', 'scrolloff', 4 )
-utils.opt('o', 'shiftround', true)
-utils.opt('o', 'smartcase', true)
--- utils.opt('o', 'splitbelow', true)
--- utils.opt('o', 'splitright', true)
-utils.opt('o', 'wildmode', 'list:longest')
-utils.opt('w', 'number', true)
-utils.opt('w', 'relativenumber', true)
-utils.opt('o', 'clipboard','unnamed,unnamedplus')
 
-utils.opt('o', 'mouse', 'a')
 
 -- Highlight on yank
 cmd 'au TextYankPost * lua vim.highlight.on_yank {on_visual = false}'
