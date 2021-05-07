51a38fc086de52f405cefa94173ef593e72ee406
Mayrixon <bygsxizhen@gmail.com>
2021-05-07 16:23:15 +0100

    Add document.

--- neovim-0.5/.config/nvim/lua/utils/init.lua
+++ neovim-0.5/.config/nvim/lua/utils/init.lua
@@ -2,6 +2,11 @@ local utils = { }
 
 local scopes = {o = vim.o, b = vim.bo, w = vim.wo}
 
+--- Set vim options.
+-- For any scope value inputted, the global value will also be setted.
+-- @param scope String in the range of {o, b, w}.
+-- @param key String of the key.
+-- @param value Any kind of desire value.
 function utils.opt(scope, key, value)
     scopes[scope][key] = value
     if scope ~= 'o' then scopes['o'][key] = value end
