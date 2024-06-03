8863d09af8c910ceece2d93b86307721cfa9c021
Mayrixon <bygsxizhen@gmail.com>
2024-06-03 13:01:13 +0800

    Fix(nvim): fix comments.

--- neovim/.config/nvim/lua/plugins/coding.lua
+++ neovim/.config/nvim/lua/plugins/coding.lua
@@ -168,11 +168,7 @@ return {
   {
     "numToStr/Comment.nvim",
     event = "VeryLazy",
-    opts = {
-      pre_hook = function()
-        require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook()
-      end,
-    },
+    opts = {},
   },
 
   -- Better text-objects
